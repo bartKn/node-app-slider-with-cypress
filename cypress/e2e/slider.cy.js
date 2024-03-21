f3eba4931901248bf60656cd54f9873335c6013a
bartKn <605bart@gmail.com>
2024-03-21 19:09:50 +0100

    Added hotjar

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,6 +1,6 @@
 describe('Swiper Gallery Test', function () {
   it('Checks if second slide contains "United Kingdom"', function () {
-    cy.visit('http://app:3000');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
   });
@@ -8,7 +8,7 @@ describe('Swiper Gallery Test', function () {
 
 describe('Swiper Gallery Test', function () {
   it('Checks if third slide contains "Paris"', function () {
-    cy.visit('http://app:3000');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.wait(2000)
     cy.get('.swiper-button-next').click({ force: true });
@@ -16,3 +16,18 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+    it('Checks if the gallery is displayed correctly', function () {
+        cy.visit('http://localhost:3000');
+        // Krok 2: Sprawdź, czy główny kontener galerii jest widoczny.
+        cy.get('.swiper').should('be.visible');
+        cy.get('.swiper-button-next').click();
+        cy.wait(2000)
+        cy.get('.swiper-button-next').click();
+        cy.wait(2000)
+        cy.get('.swiper-button-next').click();
+        cy.wait(2000)
+        cy.get('.swiper-slide-active').should('contain', 'Rome');
+      });
+  });
